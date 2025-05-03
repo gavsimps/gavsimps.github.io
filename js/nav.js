172c7c49eb15045f33056518beac6de7a0935a9e
gavsimps <gavsimps@iu.edu>
2025-05-03 09:09:17 -0400

    update css later to remove template

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,21 @@
-function openNav() {
-    let side = document.getElementById("sideNav");
-    if (side.style.display == "none") {
-        side.style.display = "block";
-        side.style.width = "250px";
-    } else {
-        side.style.display = "none";
-    side.style.width = "0";
+document.addEventListener("DOMContentLoaded", function () {
+    const trigger = document.getElementById("js-menu-trigger");
+    const screen = document.getElementById("js-menu-screen");
+    const menu = document.getElementById("js-menu");
+    
+    function toggleMenu(e) {
+        e.preventDefault();
+        if (menu) menu.classList.toggle("is-visible");
+        if (screen) screen.classList.toggle("is-visible");
+        if (trigger) {
+        trigger.classList.toggle("slide");
+        trigger.classList.toggle("close");
+        }
     }
-
-}
+    
+    ["click", "touchstart"].forEach(eventType => {
+        if (trigger) trigger.addEventListener(eventType, toggleMenu, { passive: false });
+        if (screen) screen.addEventListener(eventType, toggleMenu, { passive: false });
+    });
+    });
+      
